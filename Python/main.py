fd7d5b1b67c6ea562d2321a32f44db205054a8f3
albandvd <alban.david06@gmail.com>
2023-03-09 22:29:31 +0100

    Create main.py
    first step of the code withe the openning file fonction and the converter

---
+++ Python/main.py
@@ -0,0 +1,15 @@
+import struct
+
+def read_binary(octd, octf):
+    with open("ethernet.result_data_", "rb") as fic:
+        binary = fic.read()
+        return (binary[octd: octf])
+
+def convert_deci(nb):
+    dec_number = struct.unpack('>i', nb)
+    return (dec_number)
+
+var = struct.unpack('>i', read_binary(24, 28))
+print(var)
+
+print(convert_deci(read_binary(24, 28)))
